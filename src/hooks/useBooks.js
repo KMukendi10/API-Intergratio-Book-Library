65d9e861bca40ad5a9925f8fb0e2c0e8f8025d58
KMukendi10 <kazadishadrackmukendi@gmail.com>
2026-07-22 20:17:18 +0200

    Adding fetch logic to useBooks hook

--- src/hooks/useBooks.js
+++ src/hooks/useBooks.js
@@ -5,10 +5,33 @@ function useBooks() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
+  async function fetchBooks(search) {
+    setLoading(true);
+    setError("");
+
+    try {
+      const response = await fetch(
+        `https://openlibrary.org/search.json?q=${search}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch books.");
+      }
+
+      const data = await response.json();
+      setBooks(data.docs);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return {
     books,
     loading,
     error,
+    fetchBooks,
   };
 }
 
