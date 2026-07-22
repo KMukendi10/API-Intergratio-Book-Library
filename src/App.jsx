b15d0aa909918cc0a77f3ea1a0cc512c186de4aa
KMukendi10 <kazadishadrackmukendi@gmail.com>
2026-07-22 20:26:24 +0200

    Connect custom hook to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+import useBooks from "./hooks/useBooks";
+
 function App() {
+  const [search, setSearch] = useState("");
+
+  const { books, loading, error, fetchBooks } = useBooks();
+
+  const handleSearch = () => {
+    if (search.trim() !== "") {
+      fetchBooks(search);
+    }
+  };
+
   return (
-    <div>
+    <div className="app">
       <h1>📚 Book Search App</h1>
+
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for a book..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <button onClick={handleSearch}>Search</button>
+      </div>
+
+      {loading && <p>Loading books...</p>}
+
+      {error && <p>{error}</p>}
     </div>
   );
 }
